84510b07ca9bdbf18fae70b212a44519484f8aae
Rohan Garg <rohangarg@DN0a1ea81a.SUNet>
2024-11-06 23:59:56 -0800

    preprocessing steps wip

--- src/data/datamodule.py
+++ src/data/datamodule.py
@@ -8,16 +8,18 @@ class DyanmicsDataset(Dataset):
     def __init__(
         self,
         config,
-        data_dict=Dict[str, Dict[str, Dict[str, str]]],
+        data_dict=Dict[str, Dict[str, Dict[str, str]]],     ## data_dict is like instance_name then the modality name and then file name for data amd timestamp
         is_train=False,
     ):
-        self.config=config
-        self.modalities=config["data"]["modalities"].keys()
-        self.modality_file_list_data = {key:[] for key in self.modalities}
+        self.config=config                      # config file
+        self.modalities=config["data"]["modalities"].keys()             # all the modalities
+        self.modality_file_list_data = {key:[] for key in self.modalities}      
         self.timestamp_file_list_data = {key:[] for key in self.modalities}
         self.modality_processed_data = {key:[] for key in self.modalities}
         self.data_dict = data_dict
         self.is_train = is_train
+        self.dt = self.config['data']['dt']
+        self.horizon = self.config['data']['horizon']
         self.load_data()
 
     def load_data(self):
@@ -27,9 +29,10 @@ class DyanmicsDataset(Dataset):
         # save each modality in an list of numpy objects
         for folder_name in self.data_dict:
             for modality in self.modalities:
-                data_path = self.data_dict[folder_name][modality]["data"]
-                timestamp_path = self.data_dict[folder_name][modality]['timestamp']
-                
+                data_string = 'data'
+                filepath_string = 'filepath'
+                data_path = self.data_dict[folder_name][modality][data_string]
+                timestamp_path = self.data_dict[folder_name][modality][filepath_string]                
                 data = np.load(data_path)
                 timestamps = np.loadtxt(timestamp_path, dtype=np.float64)
                 timestamps = timestamps - timestamps[0]
@@ -38,20 +41,33 @@ class DyanmicsDataset(Dataset):
 
         # Todo: Do we need timestamps???
         for modality in self.modalities:
-            samples_in_window = int(self.config["data"]["dt"]*self.config["data"]["modalities"][modality]["frequency"])
-            for single_file_data in self.modality_file_list_data[modality]:
-                n, k = single_file_data.shape
-                reshaped_data = single_file_data[:n - (n % samples_in_window)].reshape(-1, samples_in_window, k)
-                averaged_data = reshaped_data.mean(axis=1)
-                self.modality_processed_data[modality].append(averaged_data)
-            self.modality_processed_data[modality] = np.vstack(self.modality_processed_data[modality])
+            samples_in_window = int(self.dt*self.config["data"]["modalities"][modality]["frequency"])     ## how many datapoints for different modalities in dt time
+            for single_file_data in self.modality_file_list_data[modality]:         #for a modality single_file_data would be data of that modality and instance
+                n, k = single_file_data.shape                                       # num_datapints, dim
+                ## REMOVING THE EXTRA DATA POINTS from the end of the data 
+                reshaped_data = single_file_data[:n - (n % samples_in_window)].reshape(-1, samples_in_window, k)            # removing the extra timesteps and making it in an array
+                averaged_data = reshaped_data.mean(axis=1)                                                                  # in dt time we take the average value of the data
+                self.modality_processed_data[modality].append(averaged_data)                                                # array of modality data for the entire instance
+            self.modality_processed_data[modality] = np.vstack(self.modality_processed_data[modality])                      # combining array of modality for all instances.
+
+    # def form_input_output(self):
+    #     self.
+    #     for modality in self.modalities:
 
 
     def __len__(self):
-        return list(self.modality_processed_data.values())[0].shape[0]
+        return (list(self.modality_processed_data.values())[0].shape[0] - self.horizon/self.dt - 1)                             ## no of datapoints for each modality is now the same
 
     def __getitem__(self, idx):
-        # return idx row from each modality in self.modality_processed_data
-        # return [idx:idx+horizon*dt] for ground_truth
-        # drop last few rows for all modalities as there wouldnt be enough horizon for them
-        return {modality: data[idx] for modality, data in self.modality_processed_data.items()}
+        # return idx row from each modality in self.modality_processed_data, so idx is the index at take the current state
+        
+        current_state = {}
+        environment = {}                ## rgb etc?
+        ground_truth = {}
+        for modality in self.modalities:
+            if(self.config['data']['modalities'][modality]['type'] == 'state'):
+                current_state[modality] = self.modality_processed_data[modality][idx]
+                ground_truth[modality] = self.modality_processed_data[modality][idx+1 : idx+1 + (self.horizon / self.dt)]
+            if(self.config['data']['modalities'][modality]['type'] == 'action'):
+                action_horizon = self.modality_processed_data[modality][idx : idx + (self.horizon / self.dt)]
+        return current_state, environment, action_horizon, ground_truth
--- src/data/datasplit.py
+++ src/data/datasplit.py
@@ -6,7 +6,20 @@ class DataSplitUtils:
         self.dataset_path = dataset_path
         self.folder_names = [f for f in os.listdir(self.dataset_path)]     
         self.data_dict = dict.fromkeys(self.folder_names)
-    def load_data(self, modalities):
+    def load_data(self, modalities):           
+            
+        """Loads data from the dataset and organizes it into a dictionary.
+        This function traverses the dataset directory, identifies files with a '.npy' extension,
+        and organizes them into a dictionary based on the specified modalities. Each entry in the
+        dictionary contains the absolute path to the data file and the corresponding timestamp file. 
+
+        Args:
+            modalities (list of str): List of modalities to be included in the dataset.
+
+        Returns:
+            dict: A dictionary where the key is the instance name and the value is another dictionary
+                  containing the data file path and timestamp file path.
+        """
         for folder_name in self.data_dict:
             self.data_dict[folder_name] = {}
         for dirs, _, files in os.walk(self.dataset_path):
@@ -21,6 +34,18 @@ class DataSplitUtils:
         return self.data_dict
 
     def random_train_test_split(self, test_size):
+        """
+        Splits the dataset into random train, test, and validation sets.
+        Args:
+            test_size (float): Proportion of the dataset to include in the test split. 
+                               Should be between 0.0 and 1.0.
+        Returns:
+            tuple: A tuple containing three dictionaries:
+                - train_data (dict): Training data split.
+                - test_data (dict): Testing data split.
+                - val_data (dict): Validation data split.
+        """
+        
         folders = list(self.data_dict.keys())
         np.random.shuffle(folders)
         split_index = math.floor(len(folders) * test_size)
